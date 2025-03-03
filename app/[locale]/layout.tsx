8be9de8466380721691c4691a0563ef0fc35bc36
LaurentiuPetran <emil.petran@heyblu.ro>
2025-03-03 14:29:04 +0200

    updated root layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
-import { getMessages, unstable_setRequestLocale } from "next-intl/server";
+import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { locales } from "@/i18n/routing";
+import { routing } from "@/i18n/routing";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,22 +26,17 @@ export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
 }
 
-export default async function RootLayout({
+export default async function LocaleLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
-  const locale = params.locale;
-
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as "ro" | "en")) {
+  const { locale } = await params;
+  if (!routing.locales.includes(locale as "ro" | "en")) {
     notFound();
   }
-
-  // Enable static rendering
-  unstable_setRequestLocale(locale);
 
   const messages = await getMessages();
 
